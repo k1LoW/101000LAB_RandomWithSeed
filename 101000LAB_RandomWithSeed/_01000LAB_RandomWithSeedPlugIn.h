d6e3797ac52743cb60c134ff0b29d2267345cc87
k1LoW <k1lowxb@gmail.com>
2014-02-28 21:17:08 +0900

    Initial Commit

---
+++ 101000LAB_RandomWithSeed/_01000LAB_RandomWithSeedPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  _01000LAB_RandomWithSeedPlugIn.h
+//  101000LAB_RandomWithSeed
+//
+//  Created by k1LoW on 2014/02/28.
+//  Copyright (c) 2014年 101000LAB. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface _01000LAB_RandomWithSeedPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
